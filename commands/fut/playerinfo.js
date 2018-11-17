01abde5c0b8227ffc720750cb5b1ef39e4ab69c8
adam j. sontag <ajpiano@ajpiano.com>
2018-11-17 02:45:56 -0500

    Player info command: Prevent certain Robin exclamations from showing up in sent messages

--- commands/fut/playerinfo.js
+++ commands/fut/playerinfo.js
@@ -8,6 +8,10 @@ const exclamations = require('exclamation');
 const rarities = require('../../lib/futitemraritytunables').rarities;
 const version = require('../../package').version;
 
+// Apparently this is something Robin said during the 1960s Batman TV show. However, it's not something that should be flying into discord price messages for FUT players.
+exclamations.all = _.without(exclamations.all, 'Holy Holocaust');
+exclamations.random = () => exclamations.all[Math.floor(Math.random() * exclamations.all.length)];
+
 countries.push({code: 'NL', name: 'Holland'});
 
 async function searchFutDB(term) {
